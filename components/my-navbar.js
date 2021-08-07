71fe46ae265d85397abcf2e13a01cedb256afd6a
Phill Anderson <eaglesquad86@gmail.com>
2021-08-07 19:23:19 +0800

    Context - ийг сайжруулж, layout дээрээс dark theme - ийг ажилладаг болгоно

--- components/my-navbar.js
+++ components/my-navbar.js
@@ -1,18 +1,20 @@
-import React, { useContext } from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import ThemeContext from '../context/theme-context'
+import { useTheme } from "../hooks/use-theme"
+import Link from 'next/link'
 export default () => {
-  const context = useContext(ThemeContext)
+  const { theme, toggleTheme } = useTheme()
   return (
     <Navbar className="fj-navbar fj-nav-base" bg="transparent" expand="lg">
       <Navbar.Brand className="fj-navbar-brand">
-        <a href="#" onClick={context.toggleTheme} style={{ color: context.theme.fontColor }}>1234 БЛОГ</a>
+        <Link href="/">
+          <a onClick={toggleTheme} style={{ color: theme.fontColor }}>1234 БЛОГ</a>
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link className="fj-navbar-item fj-navbar-link" href="/">
-            {context.theme.fontColor}
+            НҮҮР
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
