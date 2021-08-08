efee8735fb6f37e212ddb23cd7a27673e3b43b2d
Phill Anderson <eaglesquad86@gmail.com>
2021-08-08 10:54:26 +0800

    Preview горимыг бүрэн ажилладаг болгох, Sanity token бүхий шинэ клиент үүсгэх

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,21 +7,31 @@ import { urlFor } from "lib/api";
 import PostHeader from "components/post-header";
 import { useRouter } from "next/router"
 
-export default ({ post }) => {
+export default ({ post, preview }) => {
   const router = useRouter()
 
   // getStaticPaths - ийн fallback - ийг true болгосноор getStaticPaths - аар орж ирсэн paths -уудаас ялгаатай path бүхий хуудсыг client талаас дуудвал түүнийг ssr хийж харуулна
   // өөрөөр хэлбэл энэхүү хуудсанд SG болон SSR - ийг хослуулан хэрэглэсэн хуудас юм.
+  //fallBack горим нь сервэрээс өгөгдөл татах үеийн горим юм
   if (router.isFallback) return (
     <Layout>
       <div>түр хүлээнэ үү</div>
     </Layout>
   )
 
+  // өгөгдөл татаж дууссан үед
+  if (!router.isFallback && !post?.slug)
+    return (
+      <Layout>
+        <div>Уучлаарай ийм өгөгдөл байхгүй!</div>
+      </Layout>
+    )
+
   return (
     <Layout>
       <Row>
         <Col md="12">
+          {preview && <div>Та preview горимд байна</div>}
           <pre>{/*JSON.stringify(post, null, 2)*/}</pre>
           <PostHeader post={post} />
           <br />
@@ -55,11 +65,16 @@ const serializers = {
   },
 };
 
-export const getStaticProps = async ({ params }) => {
-  const post = await getPostBySlug(params.slug);
+export const getStaticProps = async ({ params, preview = false, previewData }) => {
+  const post = await getPostBySlug(params.slug, preview);
+
+  // post[0] - sanity-гаас published горимд байгаа post 
+  // post[1] - sanity-гаас preview горимд байгаа post
+  // post.length > 1 preview горим дээр post - д өөрчлөлт орсон эсхийг шалгаж байна
   return {
     props: {
-      post: post[0],
+      post: post.length > 1 ? post[1] : (post.length > 0 ? post[0] : {}),
+      preview,
     },
   };
 };
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -5,5 +5,7 @@ export default async (req, res) => {
     if (process.env.SANITY_PREVIEW_SECRET !== req.query.secret)
         return res.status(401).json({ message: "invalid request" + req.query.secret })
 
-    res.status(200).json({ message: "success" + req.query.slug })
+    res.setPreviewData({ data: req.query.secret })
+    res.writeHead(307, { Location: encodeURI(`/${req.query.slug}`) })
+    res.end()
 }
